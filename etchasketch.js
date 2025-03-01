c156a784d0c89ac3809e727fcd4421fb0d6391ee
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-02-28 21:33:33 -0800

    Make button event erase previous grid before creating new grid

--- etchasketch.js
+++ etchasketch.js
@@ -4,11 +4,18 @@ let gridSize = 4;
 const button = document.getElementById("button");
 button.addEventListener("click", () => {
     gridSize = prompt("Enter your desired height: ", "Enter height here");
+    createGrid(gridSize);
 });
 
 const createGrid = (gridSize) => {
     const container = document.querySelector(".container");
-    
+    let child = container.lastElementChild; //get last child in list
+
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+    //  Remove previous grid to create new one
 
     for (let i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
