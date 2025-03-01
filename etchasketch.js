3953e9fab83faa0a981c063aba55ceadea7f48e9
dsant2119 <143536409+dsant2119@users.noreply.github.com>
2025-02-28 21:09:15 -0800

    Remove console logs

--- etchasketch.js
+++ etchasketch.js
@@ -3,18 +3,14 @@
 let gridSize = 4;
 
 const createGrid = (gridSize) => {
-    // console.log("Create grid was called!");
     const container = document.querySelector(".container");
 
     for (let i = 0; i < gridSize; i++) {
-        // console.log("Creating a row!");
         const row = document.createElement("div");
         row.classList.add("row");
-        // row.textContent = "THIS IS A ROW";
         container.appendChild(row);
 
         for (let i = 0; i < gridSize; i++) {
-            // console.log("Creating cells!");
             const cell = document.createElement("div");
             cell.classList.add("cell");
             cell.textContent = "CELL";
